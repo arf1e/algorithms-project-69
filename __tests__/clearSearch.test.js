448b50cabe095a47c69eec23642820ad4488594e
Egor Bulgakov <anotheruselessbox@gmail.com>
2024-12-21 23:25:28 +0200

    feat: add token processing

--- __tests__/clearSearch.test.js
+++ __tests__/clearSearch.test.js
@@ -2,8 +2,13 @@ import clearSearch from '../src';
 import { docs } from './__fixtures__/docs';
 
 describe('clearSearch', () => {
-  it('should clear search', () => {
+  it('should perform clear search', () => {
     const search = clearSearch(docs, 'shoot');
     expect(search).toEqual(['doc1', 'doc2']);
+  });
+
+  it('should omit punctuation marks', () => {
+    expect(clearSearch(docs, 'pint')).toEqual(['doc1']);
+    expect(clearSearch(docs, 'pint!')).toEqual(['doc1']);
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
+const processToken = (token) => {
+  const term = token.match(/\w+/g);
+  return term;
+};
+
+const isFullyIntersected = (arrayA, arrayB) => {
+  const [smallerArray, largerArray] = [arrayA, arrayB].sort(
+    (a, b) => a.length - b.length
+  );
+
+  for (const item of smallerArray) {
+    if (!largerArray.includes(item)) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
 const clearSearch = (documents, word) => {
+  const searchTerm = processToken(word);
   const result = [];
   for (const document of documents) {
     const { text, id } = document;
-    const words = text.split(' ');
-    if (words.includes(word)) {
+    const terms = processToken(text);
+    if (isFullyIntersected(terms, searchTerm)) {
       result.push(id);
     }
   }
